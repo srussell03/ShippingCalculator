fe0eeffc2ddb0537b5ee2799a71bce33a221da74
Seth <seth.ru4116@vusd.us>
2020-09-19 19:13:31 -0700

    This is the first commit to the Shipping Calculator project.

---
+++ app/src/main/java/com/example/shippingcalculator/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.shippingcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText weightET;
+    ShipItem shipItem;
+    TextView baseTV, addedTV, totalTV;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        weightET = (EditText)findViewById(R.id.editText_weight);
+        shipItem = new ShipItem();
+        baseTV = (TextView)findViewById(R.id.base_cost_textView);
+        addedTV = (TextView)findViewById(R.id.added_cost_textView);
+        totalTV = (TextView)findViewById(R.id.total_cost_textView);
+
+        weightET.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //leave empty
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                try{
+                    shipItem.setWeight(Double.parseDouble(charSequence.toString()));
+                }catch(NumberFormatException e){
+                    shipItem.setWeight(0.0);
+                }
+                displayShipping();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //Leave empty
+            }
+        });
+    }
+    public void displayShipping(){
+        @SuppressLint("DefaultLocale") String base = String.format("$ %.2f",shipItem.getBaseCost());
+        @SuppressLint("DefaultLocale") String added = String.format("$ %.2f",shipItem.getAddedCost());
+        @SuppressLint("DefaultLocale") String total = String.format("$ %.2f",shipItem.getTotalCost());
+        baseTV.setText(base);
+        addedTV.setText(added);
+        totalTV.setText(total);
+    }
+}
---
+++ app/src/main/java/com/example/shippingcalculator/ShipItem.java
@@ -0,0 +1,41 @@
+package com.example.shippingcalculator;
+
+public class ShipItem {
+    Double weight;
+    static final Double BASE = 3.00;
+    static final Double ADDED = 0.50;
+    static final double EXTRA = 4.00;
+    static final int BASE_WEIGHT = 16;
+    static final double EXTRA_WEIGHT = 4.0;
+
+    public ShipItem() {
+        weight = 0.0;
+    }
+    public ShipItem(Double weight){
+        this.weight = weight;
+    }
+    public Double getBaseCost(){
+        //include implementation
+        if(weight > EXTRA_WEIGHT) return EXTRA;
+        return BASE;
+
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getAddedCost(){
+        //include implementation
+        if(weight > BASE_WEIGHT) {
+            double overage = (weight - BASE_WEIGHT);
+            return Math.ceil(overage / 4 * ADDED);
+        }
+        return 0.0;
+    }
+
+    public Double getTotalCost(){
+        return getBaseCost() + getAddedCost();
+    }
+
+}
